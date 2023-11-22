da3b7a4dc5b451ad6a7f91d4717120f4e09468c2
Zinmus <alex.zinmus@gmail.com>
2023-11-22 14:47:51 +0100

    Frontend Builder update

--- src/views/FrontendEditorView.vue
+++ src/views/FrontendEditorView.vue
@@ -9,6 +9,14 @@
     </div> -->
 
     <DOMRenderer :node="dom"/>
+    <button @click="addDiv" class="bg-blue text-black mr-4 h-8 px-2">
+      <!-- <svg-icon name="plus-small-blue" class="h-4 w-4" /> -->
+      Add div
+    </button>
+    <button @click="showDOMLog" class="bg-blue text-black mr-4 h-8 px-2">
+      <!-- <svg-icon name="clock" class="h-4 w-4" /> -->
+      Show DOM log
+    </button>
   </div>
 </template>
 
@@ -19,86 +27,29 @@ import DOMRenderer from '@/components/DOMRenderer.vue';
 import SpinnerLoader from '@/components/SpinnerLoader.vue';
 
 const router = useRouter()
+const newDivId = ref(0)
 
-const dom = {
+function addDiv(){
+  dom.value.children.push({
+    tag: 'div', 
+    innerText: 'New div ' + newDivId.value.toString(),
+    attrs: {
+      id: 'newDiv' + newDivId.value.toString(), 
+      class: 'resize overflow-scroll bg-blue text-black',
+    }})
+
+  newDivId.value++;
+}
+
+const dom = ref({
   tag:'div',
-  children: [
-    {
-      tag: 'div',
-      children: [
-        {
-          tag: 'p',
-          innerText: 'Header',
-          attrs: {
-            class: 'text-2xl mb-5'
-          }
-        }
-      ]
-    },
-    {
-      tag:'div',
-      children: [
-        {
-          tag: 'p',
-          innerText: 'Content'
-        }
-      ]
-    },
-    {
-      tag:'div',
-      children: [
-        {
-          tag: 'p',
-          innerText: 'Spinner',
-          attrs: {
-            class: 'text-xs'
-          }
-        },
-        {
-          tag: SpinnerLoader,
-        }
-      ]
-    },
-    {
-      tag:'div',
-      children: [
-        {
-          tag: 'p',
-          innerText: 'Footer'
-        }
-      ]
-    }
-  ]
-}
+  attrs: {
+    class: 'h-screen'
+  },
+  children: [{}]
+})
 
-const object = {
-  id: 1,
-  type: 'div',
-  content: 'test',
-  style: {
-    width: '100px',
-    height: '100px',
-    'background-color': 'lime'
-  }
-}
-
-const divs = ref([
-  {
-    id: 0,
-    type: 'div',
-    content: 'test',
-    style: {
-      width: '100px',
-      height: '100px',
-      'background-color': 'blue'
-    }
-  }
-])
-
-function addObject() {
-  if (object.type == 'div') {
-    divs.value.push(object)
-    object.id += 1
-  }
+function showDOMLog(){
+  console.log(dom.value.children);
 }
 </script>
