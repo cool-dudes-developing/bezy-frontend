a7d6c0e938e8e138c93f0a30875c5c87c5ec1921
Zinmus <alex.zinmus@gmail.com>
2024-01-25 13:47:35 +0100

    Router home route update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import User from '@/models/User'
 import EditorSidebar from '@/layouts/EditorSidebar.vue'
 import ProjectSidebar from '@/layouts/ProjectSidebar.vue'
@@ -9,7 +8,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    redirect: { name: 'platform'}
   },
   {
     path: '/about',
