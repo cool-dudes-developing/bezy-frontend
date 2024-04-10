8f528e4aaa70b1e873240be2066e9ae41cc5a50c
Zinmus <alex.zinmus@gmail.com>
2024-04-10 03:30:14 +0200

    hover simple transition added

--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -1,5 +1,5 @@
 <template>
-    <tr class="bg-transparent backdrop-brightness-75 drop-shadow-sm border border-blue hover:backdrop-brightness-50 cursor-pointer">
+    <tr class="bg-transparent backdrop-brightness-75 drop-shadow-sm border border-blue hover:backdrop-brightness-50 cursor-pointer duration-300 ease-in-out">
         <slot></slot>
     </tr>
 </template>
